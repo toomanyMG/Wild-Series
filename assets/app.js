3eb5b43d0da56f21ae8a4d2117cd14bcbd260a14
toomanyMG <tchiki.tshiki@gmail.com>
2022-11-28 11:35:42 +0100

    Added the navbar

--- assets/app.js
+++ assets/app.js
@@ -5,17 +5,28 @@
  * (and its CSS file) in your base layout (base.html.twig).
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
+// the ~ allows you to reference things in node_modules
 import './styles/root.scss';
 import './styles/app.scss';
 
 // start the Stimulus application
 import './bootstrap';
 
-console.log('Hello Webpack Encore !');
 
 /* // returns the final, public path to this file
 // path is relative to this file - e.g. assets/images/logo.png
 import logoPath from '../images/logo.png';
 
 let html = `<img src="${logoPath}" alt="ACME logo">`; */
+const $ = require('jquery');
+// this "modifies" the jquery module: adding behavior to it
+// the bootstrap module doesn't export/return anything
+require('bootstrap');
+
+// or you can include specific pieces
+// require('bootstrap/js/dist/tooltip');
+// require('bootstrap/js/dist/popover');
+
+$(document).ready(function() {
+    $('[data-toggle="popover"]').popover();
+});
